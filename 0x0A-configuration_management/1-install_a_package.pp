044ee2e1450ee2e24d2e5ff3ae3a38987b4936ad
Francis Omiti <112470574+omiti-franc@users.noreply.github.com>
2023-03-28 09:20:11 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,20 @@
-# install puppet-lint
+# Install a package using Puppet
+
+exec { 'install ruby':
+  command => '/usr/bin/apt-get install -y ruby'
+}
+
+exec { 'install puppet-lint':
+  command => '/usr/bin/gem install puppet-lint -v 2.1.1'
+}
+
+package { 'ruby':
+  ensure => 'installed',
+  before => Exec['install ruby']
+}
+
 package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem'
+  ensure  => 'installed',
+  before  => Exec['install puppet-lint'],
+  require => Package['ruby']
 }
