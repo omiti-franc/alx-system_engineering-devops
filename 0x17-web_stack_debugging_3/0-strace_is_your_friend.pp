1f4788695a996ae680261083d7ed658327fc1aa4
Francis Omiti <112470574+omiti-franc@users.noreply.github.com>
2023-06-27 13:21:49 +0300

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,5 +0,0 @@
-# Fourth web-stack debug script in puppet
-exec { 'wsd3':
-  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
-  path    =>  ['/bin', '/usr/bin']
-}
